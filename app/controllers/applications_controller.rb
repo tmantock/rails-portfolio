1402d97ee3c0a77c0e9c25699131f3d4afede0e5
Tevin Mantock <tmantock@gmail.com>
2017-08-25 18:51:26 -0700

    form updates

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,4 +4,27 @@ class ApplicationsController < ApplicationController
         # Get all of the applications
         @applications = Application.all
     end
+
+    # New Route for the Application form
+    def new
+        @application = Application.new
+    end
+
+    # Create Method for creating a new Application
+    def create
+        @application = Application.new(application_params)
+
+        respond_to do |format|
+            if @application.save
+                format.html { redirect_to applications_path, notice: 'Your Application has been created.'}
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+    private
+        def application_params
+            params.require(:application).permit(:title, :subtitle, :thumbnail, :image, :body)
+        end
 end
