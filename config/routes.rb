f6ae25ee5d6881e0a11d9024d652c5c102c1f485
Tevin Mantock <tmantock@gmail.com>
2017-08-26 12:46:20 -0700

    Custom routes for basic pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
   resources :applications
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
   resources :blogs
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
