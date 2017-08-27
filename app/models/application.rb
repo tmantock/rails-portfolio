86eaf20482ea6227f0bc83113c44f187d0831aaf
Tevin Mantock <tmantock@gmail.com>
2017-08-27 11:50:28 -0700

    Placeholder concern for generating a default image placeholder

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,4 +1,6 @@
 class Application < ApplicationRecord
+    include Placeholder
+
     validates_presence_of :title, :image, :thumbnail, :body
 
     def self.ember
@@ -6,4 +8,11 @@ class Application < ApplicationRecord
     end
 
     scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails")}
+
+    after_initialize :set_defaults
+
+    def set_defaults
+        self.image ||= Placeholder.image_generator(height: '600', width: '400')
+        self.thumbnail ||= Placeholder.image_generator(height: '350', width: '200')
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170827173615) do
+ActiveRecord::Schema.define(version: 20170827184112) do
 
   create_table "applications", force: :cascade do |t|
     t.string "title"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170827173615) do
     t.integer "utilization"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "badge"
   end
 
   create_table "topics", force: :cascade do |t|
