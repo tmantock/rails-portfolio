1391cf27b90ee59eae8766bc0091a656cad5009d
Tevin Mantock <tmantock@gmail.com>
2017-09-08 18:14:28 -0700

    change method name

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -2,10 +2,10 @@ module DefaultPageContent
 	extend ActiveSupport::Concern
 
 	included do
-		before_action :set_site_title
+		before_action :set_page_defaults
 	end
 
-	def set_site_title
+	def set_page_defaults
 	  	@page_title = "Tevin Mantock | Software Engineer"
   	end
   end
