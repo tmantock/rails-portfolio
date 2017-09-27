d42afe34ba183c98574c2c3c6da352ef161deb9c
Tevin Mantock <tmantock@gmail.com>
2017-09-26 17:03:48 -0700

    Update wrong spacing

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,15 +2,15 @@ module CurrentUserConcern
 	extend ActiveSupport::Concern
 
 	def current_user
-  		super || friendly_user
-  	end
+  	super || friendly_user
+	end
 
-  	def friendly_user
-  		OpenStruct.new(
-  			name: "Friend User",
-  			firstname: "Friend",
-  			lastname: "User",
-  			email: "friend@example.com"
-  		)
-  	end
+  def friendly_user
+  	OpenStruct.new(
+  		name: "Friend User",
+  		firstname: "Friend",
+  		lastname: "User",
+  		email: "friend@example.com"
+  	)
+  end
 end
